99f227677db7b9cececc16f12ead5b3e07953b91
Daniel de la Peña <danieldelapena@X22087.local>
2024-01-09 17:53:46 +0100

    Logger middleware working on responses

--- src/shared/infraestructure/middleware/logger.middleware.ts
+++ src/shared/infraestructure/middleware/logger.middleware.ts
@@ -6,7 +6,21 @@ import { CustomLogger } from '../logging/custom.logger';
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new CustomLogger(`HTTP`);
   use(req: Request, res: Response, next: NextFunction) {
-    this.logger.debug(`Request: ${req.method}  ${req.originalUrl}`);
+    this.logger.debug(
+      `[REQUEST] Method: ${req.method}  Url: ${req.originalUrl}`,
+    );
+
+    res.on('close', () => {
+      this.logger.debug(
+        `
+        [RESPONSE]
+        Method: ${req.method}  
+        Url: ${req.originalUrl} 
+        ResponseCode: ${res.statusCode}
+        `,
+      );
+    });
+
     next();
   }
 }
