862d909751ffbb758ec24add5a8214f3c8fd8c76
Daniel de la Peña <danieldelapena@X22087.local>
2024-01-09 13:37:35 +0100

    Error filter and custom logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ErrorFilter } from './shared/infraestructure/filter/error.filter';
+import { CustomLogger } from './shared/infraestructure/logging/custom.logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new CustomLogger(),
+  });
+
+  app.useGlobalFilters(new ErrorFilter());
+
   const config = new DocumentBuilder()
     .setTitle('Nest Seed')
     .setDescription('The Nest Seed API description')
@@ -11,6 +18,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/shared/infraestructure/filter/error.filter.ts
@@ -0,0 +1,36 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CustomLogger } from '../logging/custom.logger';
+
+@Catch()
+export class ErrorFilter implements ExceptionFilter {
+  private logger = new CustomLogger();
+  catch(exception, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    let status = 500;
+    let res = {
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      statusCode: 500,
+    };
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      res.statusCode = status;
+    }
+
+    const extraLogInfo = {
+      stack: exception.stack,
+      message: exception.message,
+    };
+
+    this.logger.error(JSON.stringify({ res, extraLogInfo }));
+    response.status(status).json(res);
+  }
+}
