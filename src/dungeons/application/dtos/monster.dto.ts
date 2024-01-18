4a8825747df77179f08070d77dc07a8b9b146a23
Daniel de la Peña <danieldelapena@X22087.local>
2024-01-18 11:06:53 +0100

    Using swagger plugin to avoid framework references in the application layer

--- src/dungeons/application/dtos/monster.dto.ts
+++ src/dungeons/application/dtos/monster.dto.ts
@@ -1,30 +1,15 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 export class MonsterDto {
-  @ApiProperty({
-    type: String,
-    example: 'Acolyte',
-  })
   name: string;
 
-  @ApiProperty({
-    type: String,
-    example: 'red-acolyte',
-  })
   index: string;
-
-  @ApiProperty({
-    type: Number,
-    example: 1,
-  })
+  /**
+   * @example '0220'
+   */
   hitPoints: number;
 
-  @ApiProperty({
-    type: Number,
-    example: 10,
-  })
   hitPointsMultiplied: number;
 
+  //TODO: change this for a contstructor, use new MonsterDto(monster) in usecase
   public static entityToDto(monster: Monster): MonsterDto {
     return {
       name: monster.name,
